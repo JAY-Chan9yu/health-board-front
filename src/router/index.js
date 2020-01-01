89f86dcea07f0986621c245e9cb4912759f36c98
지찬규 <ckj9014@gmail.com>
2020-01-01 23:23:19 +0900

    [router] index.js 내용 정리 (라우터 새로 추가함 <- 재정리 필요)

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,4 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Detail from '@/components/Detail'
-import TodoPage from '@/components/TodoPage'
 
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/detail',
-      name: 'Detail',
-      component: Detail
-    },
-    {
-      path: '/todos',
-      name: 'TodoPage',
-      component: TodoPage
-    }
-  ]
-})
